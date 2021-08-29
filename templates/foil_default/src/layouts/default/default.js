2e278a0bb79ed661272fc1a34118b0dd2b5d4b18
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-29 23:36:51 +0700

    Menerapkan navigasi melalui router

--- templates/foil_default/src/layouts/default/default.js
+++ templates/foil_default/src/layouts/default/default.js
@@ -1,13 +1,19 @@
 import { defineComponent, ref } from 'vue'
-import { useRoute } from "vue-router"
+import { useRoute, useRouter } from "vue-router"
 
 export default defineComponent({
   name: 'MainLayout',
   setup () {
     const route = useRoute()
+    const router = useRouter()
+
+    const gotoPage = async (name) => {
+      await router.push({name})
+    }
 
     return {
-      route
+      route,
+      gotoPage
     }
   }
 })
