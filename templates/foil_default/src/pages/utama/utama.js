7f3532fd3bf54269b994f2cf6060250a8becc092
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-08-30 19:41:33 +0700

    Membuat fungsi interaksi awal halaman utama

--- templates/foil_default/src/pages/utama/utama.js
+++ templates/foil_default/src/pages/utama/utama.js
@@ -1,5 +1,33 @@
-import { defineComponent } from 'vue'
+import {defineComponent, ref} from 'vue'
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+  setup() {
+    const airfoilForm = ref(null)
+    const sudut = ref(0)
+    const koordinat = ref("")
+
+    const onProsesKoordinat = () => {
+      airfoilForm.value.validate().then(success => {
+        if (success) {
+          console.log(sudut.value)
+          console.log(koordinat.value)
+        }
+      })
+    }
+
+    const onResetKoordinat = () => {
+      airfoilForm.value.resetValidation()
+      sudut.value = 0
+      koordinat.value = ""
+    }
+
+    return {
+      sudut,
+      koordinat,
+      airfoilForm,
+      onResetKoordinat,
+      onProsesKoordinat
+    }
+  }
 })
