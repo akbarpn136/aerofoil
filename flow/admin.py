a1bd83dafc9f325ca527de87fe2697c86da8da35
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-09-09 18:27:28 +0700

    Mendaftarkan model database koleksi airfoil ke dashboard

--- flow/admin.py
+++ flow/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
+from flow.models import KoleksiAirfoil
+
 admin.site.site_header = 'Aerofoil Dashboard'
 admin.site.site_title = 'BBTA3'
 admin.site.index_title = 'Aerofoil'
+admin.site.register(KoleksiAirfoil)
