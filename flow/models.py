f4dd2205d24e57ff01d9360cfb678588c79c1c0c
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-09-09 18:15:44 +0700

    Menambahkan model database koleksi airfoil

--- flow/models.py
+++ flow/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class KoleksiAirfoil(models.Model):
+    nama = models.CharField(max_length=255)
+    re = models.FloatField()
+    alpha = models.FloatField()
+    cp_upper = models.TextField()
+    cp_lower = models.TextField()
+    cl = models.FloatField()
+    cd = models.FloatField()
+    cm = models.FloatField()
+    sdf = models.ImageField(upload_to="airfoil")
+    dibuat = models.DateTimeField(auto_now_add=True)
+    diubah = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Koleksi Airfoil"
+        ordering = ["nama", "alpha"]
+        indexes = [
+            models.Index(fields=["nama", "alpha"])
+        ]
+
+    def __str__(self):
+        return f"{self.nama} ({self.alpha})"
