caccc2f10c5e3fed24ea155442e4152de7712c7d
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-09-13 10:35:18 +0700

    Membuat fungsi awal state mutation data koleksi airfoil

--- templates/foil_default/src/store/foil/mutations.js
+++ templates/foil_default/src/store/foil/mutations.js
@@ -1,4 +1,23 @@
-export function foilMutation (state, {ori, sdf}) {
+export function foilMutation(state, {ori, sdf}) {
   state.foil.ori = ori
   state.foil.sdf = sdf
+}
+
+export function koleksiAirfoilMutation(state, {count, next, previous, results}) {
+  state.koleksi.count = count
+  state.koleksi.next = next
+  state.koleksi.previous = previous
+
+  results.forEach(item => {
+    state.koleksi.results.push(item)
+  })
+}
+
+export function resetKoleksiAirfoilMutation(state) {
+  state.koleksi = {
+    count: 0,
+    next: null,
+    previous: null,
+    results: []
+  }
 }
