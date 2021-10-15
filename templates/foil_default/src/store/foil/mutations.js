985fa418caddfdcdfe8d0e12ee111b1f736c1c9a
Arizal Akbar Zikri <akbarpn136@gmail.com>
2021-10-15 14:06:45 +0700

    Membuat fungsi state mutation koleksi nama airfoil

--- templates/foil_default/src/store/foil/mutations.js
+++ templates/foil_default/src/store/foil/mutations.js
@@ -21,3 +21,22 @@ export function resetKoleksiAirfoilMutation(state) {
     results: []
   }
 }
+
+export function namaAirfoilMutation(state, {count, next, previous, results}) {
+  state.nama_airfoil.count = count
+  state.nama_airfoil.next = next
+  state.nama_airfoil.previous = previous
+
+  results.forEach(item => {
+    state.nama_airfoil.results.push(item)
+  })
+}
+
+export function resetNamaAirfoilMutation(state) {
+  state.nama_airfoil = {
+    count: 0,
+    next: null,
+    previous: null,
+    results: []
+  }
+}
