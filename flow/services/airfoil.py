e81f3eff3084b4a33665068ffa3ed5be1d163ada
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-02-17 06:32:57 +0700

    Modifikasi cmap dan airfoil (selig atau Lednicer) format

--- flow/services/airfoil.py
+++ flow/services/airfoil.py
@@ -1,10 +1,10 @@
 import io
 import os
-
 import cv2
 import skfmm
 import base64
 import numpy as np
+import pandas as pd
 from django.conf import settings
 from rest_framework import status
 from matplotlib import pyplot as plt
@@ -30,6 +30,7 @@ class AirfoilGenerator:
     def sdf_image(angle: int, resolution: int,
                   dimension: int, points, filename: str = None,
                   gen_sdf: bool = True, save: bool = False):
+        padding = 110
         offset_y = resolution // 2
         phi = -1 * np.ones((resolution, resolution, 1), dtype="uint8")
         airfoils = np.empty((0, dimension), int)
@@ -47,11 +48,12 @@ class AirfoilGenerator:
         airfoils = airfoils.reshape((-1, 1, 2))
         cv2.fillPoly(phi, [airfoils], (255, 255, 255), lineType=cv2.LINE_AA)
         phi = cv2.flip(phi, 0)
+        phi = cv2.copyMakeBorder(phi, padding, padding, padding, padding, cv2.BORDER_CONSTANT, value=[-1, -1, -1])
         phi = AirfoilGenerator._rotate_img(phi, angle)
         plt.axis('off')
 
         if gen_sdf:
-            colormap = "plasma"
+            colormap = "jet"
             phi = skfmm.distance(phi, dx=1, order=2)
         else:
             colormap = "gray"
@@ -61,12 +63,12 @@ class AirfoilGenerator:
             name = filename if filename else "airfoil"
             curr_dir = settings.PUBLIC_DIR
             save_path = os.path.join(curr_dir, "public", "media", "airfoil", f"{name}.png")
-            plt.savefig(save_path, bbox_inches="tight", pad_inches=0, dpi=34.7)
+            plt.savefig(save_path, bbox_inches="tight", pad_inches=0)
 
             return None
         else:
             flike = io.BytesIO()
-            plt.savefig(flike, bbox_inches="tight", pad_inches=0, dpi=34.7)
+            plt.savefig(flike, bbox_inches="tight", pad_inches=0)
 
             return base64.b64encode(flike.getvalue()).decode()
 
@@ -74,10 +76,20 @@ class AirfoilGenerator:
     def parse_input(points_input, angle, pixels):
         try:
             points = points_input.strip().splitlines()
-            coords = [{
-                "x": float(point.split(maxsplit=1)[0]),
-                "y": float(point.split(maxsplit=1)[1])
-            } for point in points]
+            df = pd.DataFrame(points)
+            df = df[0].str.split(expand=True)
+            df.columns = ["x", "y"]
+            df = df.astype("float32")
+            val = df.loc[0, "x"]
+            val = int(round(val, 0))
+            if val != 1:
+                first_idx = df.loc[df.x == int(round(1, 0))].index.values
+                first_half = df.iloc[:first_idx[0] + 1]
+                second_half = df.iloc[first_idx[0] + 2:]
+                first_half = first_half.loc[::-1]
+                df = pd.concat([first_half, second_half], axis=0, ignore_index=True)
+
+            coords = df.to_dict("records")
 
             ori = AirfoilGenerator.sdf_image(angle, pixels, 2, coords, gen_sdf=False)
             img = AirfoilGenerator.sdf_image(angle, pixels, 2, coords, gen_sdf=True)
