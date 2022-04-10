01f632f3c3229fe91d90139754d5efc2ba928da9
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-10 13:48:20 +0700

    Restructuring the project

--- templates/foil_default/src/router/utama/utama_route.js
+++
@@ -1,7 +0,0 @@
-export const Utama = {
-  path: '/',
-  component: () => import('layouts/default/Default.vue'),
-  children: [
-    { path: '', component: () => import('pages/utama/Utama.vue'), name: "utama" }
-  ]
-}
