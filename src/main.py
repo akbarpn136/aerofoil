2253ff2c93e15faf0f91f77a5e79fe17a9c83677
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-11 04:37:42 +0700

    Change the app title

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 
-from src.aerofoil.routers import app_router
+from src.aerofoil.routers.base import app_router
 
 app = FastAPI(
-    title="Aerofoil",
+    title="AEROFOIL",
     version="1.0.0",
     description="Web-based applications to help users in predicting aerodynamic coefficients of airfoils."
 )
