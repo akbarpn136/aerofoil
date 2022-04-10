d59ab404a24bd7bfa203f8a415f17058cfbe225b
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-11 04:37:09 +0700

    Add new base router

--- src/aerofoil/routers/base.py
+++ src/aerofoil/routers/base.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 
+from src.aerofoil.routers.rendering import rendering_router
+
 
 app_router = APIRouter()
 
 @app_router.get("/", tags=["app"])
 async def index():
     return dict(msg="This page is intentionally blank")
+
+app_router.include_router(rendering_router)
