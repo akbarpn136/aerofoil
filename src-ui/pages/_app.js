aec43ecae3289f6411616183ee38b633dda9fe81
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-11 20:30:58 +0700

    Apply chakra ui

--- src-ui/pages/_app.js
+++ src-ui/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import {ChakraProvider, extendTheme} from "@chakra-ui/react"
+
+import DefaultTheme from "../themes/default"
+
+
+const defaultTheme = extendTheme(DefaultTheme)
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ChakraProvider theme={defaultTheme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
 }
 
 export default MyApp
