613e664e9ff82cdc64f33083b7b669966de65314
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-11 07:27:14 +0700

    Add new key to dict

--- src/aerofoil/routers/base.py
+++ src/aerofoil/routers/base.py
@@ -7,6 +7,6 @@ app_router = APIRouter()
 
 @app_router.get("/", tags=["app"])
 async def index():
-    return dict(msg="This page is intentionally blank")
+    return dict(msg="This page is intentionally blank", data=None)
 
 app_router.include_router(rendering_router)
