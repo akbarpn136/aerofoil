09f6c81533872bce88ef90f03c8b5ee399c1da56
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-12 12:30:30 +0700

    Register navbar into app

--- src-ui/pages/_app.js
+++ src-ui/pages/_app.js
@@ -1,12 +1,14 @@
 import {ChakraProvider, extendTheme} from "@chakra-ui/react"
 
 import DefaultTheme from "../themes/default"
+import AerofoilNavbar from "../components/aerofoil_navbar"
 
 
 const defaultTheme = extendTheme(DefaultTheme)
 
 function MyApp({ Component, pageProps }) {
   return <ChakraProvider theme={defaultTheme}>
+    <AerofoilNavbar />
     <Component {...pageProps} />
   </ChakraProvider>
 }
