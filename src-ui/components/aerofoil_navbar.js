3d58c33bc74a94055e0e540888f09e62d73d0082
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-12 22:55:08 +0700

    Implement NavLink component

--- src-ui/components/aerofoil_navbar.js
+++ src-ui/components/aerofoil_navbar.js
@@ -3,20 +3,19 @@ import {
     Flex,
     Link,
     HStack,
-    Button,
     VStack,
     Heading,
     IconButton,
-    useColorMode,
     useDisclosure,
     useColorModeValue,
 } from "@chakra-ui/react"
 import {
-    SunIcon,
-    MoonIcon,
     CloseIcon,
     HamburgerIcon,
 } from "@chakra-ui/icons"
+
+import AerofoilNavLink from "./aerofoil_navlink"
+import AerofoilToggleLight from "./aerofoil_togglelight"
 
 const NavLink = (props) => (
     <Link
@@ -25,58 +24,64 @@ const NavLink = (props) => (
         rounded={"md"}
         _hover={{
             textDecoration: "none",
-            bg: "teal.600",
+            bg: useColorModeValue("teal.600", "teal.800"),
         }}
         href={props.href}>
         {props.children}
     </Link>
 )
 
-export default function Simple() {
-    const links = ["Tools", "Datasets"]
+export default function AerofoilNavbar() {
+    const links = [
+        { text: "Tools", href: "/" },
+        { text: "Datasets", href: "/" },
+    ]
     const { isOpen, onOpen, onClose } = useDisclosure()
-    const { colorMode, toggleColorMode } = useColorMode()
 
     return (
         <div>
-            <Box bg={useColorModeValue("teal.500", "teal.700")} color="white" px={4} boxShadow="md">
+            <Box
+                bg={useColorModeValue("gray.50", "gray.700")}
+                px={4} boxShadow="md" position="absolute" w="full"
+            >
                 <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-                    <Heading as="h2" size="md" textShadow="1px 1px black">
+                    <Heading as="h1" size="lg">
                         ✈️ AEROFOIL
                     </Heading>
 
                     <HStack
-                        as={"nav"}
+                        as="nav"
                         spacing={4}
                         display={{ base: "none", md: "flex" }}
                     >
                         {links.map((link) => (
-                            <NavLink key={link} href="#">{link}</NavLink>
+                            <AerofoilNavLink key={link.text} href={link.href}>
+                                {link.text}
+                            </AerofoilNavLink>
                         ))}
 
-                        <Button
-                            onClick={toggleColorMode}
-                            bg={useColorModeValue("teal.500", "teal.700")}
-                            rounded="full"
-                        >
-                            {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-                        </Button>
+                        <AerofoilToggleLight />
                     </HStack>
 
-                    <IconButton
-                        size={"md"}
-                        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-                        aria-label={"Open Menu"}
-                        display={{ md: "none" }}
-                        onClick={isOpen ? onClose : onOpen}
-                    />
+                    <HStack as="nav" display={{ md: "none" }} spacing={4}>
+                        <AerofoilToggleLight />
+                        <IconButton
+                            size={"md"}
+                            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                            aria-label={"Open Menu"}
+                            bg={useColorModeValue("gray.100", "gray.800")}
+                            onClick={isOpen ? onClose : onOpen}
+                        />
+                    </HStack>
                 </Flex>
 
                 {isOpen ? (
                     <Box pb={4} display={{ md: "none" }}>
                         <VStack as={"nav"} spacing={4} align="stretch">
                             {links.map((link) => (
-                                <NavLink key={link}>{link}</NavLink>
+                                <AerofoilNavLink key={link.text} href={link.href}>
+                                    {link.text}
+                                </AerofoilNavLink>
                             ))}
                         </VStack>
                     </Box>
