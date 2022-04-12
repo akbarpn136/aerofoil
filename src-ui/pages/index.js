31f25cc72ccdafe9ef4c3c9eb65649d1d8891954
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-12 12:30:44 +0700

    Create dummy main page

--- src-ui/pages/index.js
+++ src-ui/pages/index.js
@@ -1,22 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>AEROFOIL</title>
         <meta name="description" content="AEROFOIL Simple prediction for aerodynamic coefficient of airfoil" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main>
+        <h1>
           Welcome to AEROFOIL
         </h1>
 
-        <p className={styles.description}>
+        <p>
           Web-based applications to help users in predicting aerodynamic coefficients of airfoils
         </p>
       </main>
