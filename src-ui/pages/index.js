5c6bab54280ab1919f68fbf68b3e58b8fc8c2591
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-12 22:57:44 +0700

    Continue designing main page

--- src-ui/pages/index.js
+++ src-ui/pages/index.js
@@ -1,4 +1,10 @@
 import Head from "next/head"
+import {
+  Box,
+  Text,
+  Container,
+  useColorModeValue
+} from "@chakra-ui/react"
 
 export default function Home() {
   return (
@@ -9,15 +15,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>
-          Welcome to AEROFOIL
-        </h1>
+      <Box
+        as="main"
+        pt={16}
+        minH="100vh"
+        bg={useColorModeValue("gray.100", "gray.800")}
+      >
+        <Container maxW="98%" mt={25}>
+          <Text as="h2" fontSize="2xl">
+            Airfoil Prediction
+          </Text>
 
-        <p>
-          Web-based applications to help users in predicting aerodynamic coefficients of airfoils
-        </p>
-      </main>
+          <p>
+            Web-based applications to help users in predicting aerodynamic coefficients of airfoils
+          </p>
+        </Container>
+      </Box>
     </div>
   )
 }
