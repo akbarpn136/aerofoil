ab792a7b86bb7bbb052129b97983e20deacd43fa
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-12 22:57:09 +0700

    Fix on crossorigin

--- src-ui/pages/_document.js
+++ src-ui/pages/_document.js
@@ -6,7 +6,7 @@ class AerofoilDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,700;1,400;1,700&display=swap"/>
         </Head>
         <body>
