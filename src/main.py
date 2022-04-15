8983f2df02fa5504d9d63aa19cb7c6d55d1b1247
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-15 20:56:54 +0700

    Implement CORS middleware

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.aerofoil.routers.base import app_router
 
@@ -6,6 +7,13 @@ app = FastAPI(
     title="AEROFOIL",
     version="1.0.0",
     description="Web-based applications to help users in predicting aerodynamic coefficients of airfoils."
+)
+
+app.add_middleware(CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(app_router)
