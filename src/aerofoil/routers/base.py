f1e41689b433853beab13fbd70132899686855ef
Arizal Akbar Zikri <akbarpn136@gmail.com>
2022-04-15 23:00:15 +0700

    Register new prediction router

--- src/aerofoil/routers/base.py
+++ src/aerofoil/routers/base.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .render import render_router
+from .predict import predict_router
 
 
 app_router = APIRouter()
@@ -10,3 +11,4 @@ async def index():
     return dict(msg="This page is intentionally blank", data=None)
 
 app_router.include_router(render_router)
+app_router.include_router(predict_router)
